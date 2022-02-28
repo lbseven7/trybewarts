da43f589c0fcda5676c48fe1b3a851df7cd98942
Matheus Marinho <math.marinho154@gmail.com>
2022-02-28 11:51:06 -0300

    Requisitos 18 e 19

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const inputEmail = document.querySelector('.email');
 const inputSenha = document.querySelector('.senha');
 const btnLogin = document.querySelector('#btn-login');
+const agreement = document.querySelector('#agreement');
+const btnSubmit = document.querySelector('#submit-btn');
 
 function validate() {
   if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
@@ -11,3 +13,13 @@ function validate() {
 }
 
 btnLogin.addEventListener('click', validate);
+
+function btnStatus() {
+  if (agreement.checked) {
+    btnSubmit.disabled = false;
+  } else {
+    btnSubmit.disabled = true;
+  }
+}
+
+agreement.addEventListener('click', btnStatus);
